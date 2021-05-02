3a6575906b4fe79e831d36bb9f1d9cce887c69f2
bikash007-gb <bikashmali12@gmail.com>
2021-05-03 00:49:02 +0530

    changes

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,8 +1,3 @@
-// describe('Sample Test', () => {
-//     it('should test that true === true', () => {
-//       expect(true).toBe(true)
-//     })
-//   })
 
 const request = require('supertest')
 const app = require('../app')
--- tests/setup.js
+++ tests/setup.js
@@ -5,8 +5,16 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 jest.setTimeout(600000)
 
+// mongoose
+//   .connect('mongodb://127.0.0.1:27017/test_db', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log('DB connection successful!'));
 mongoose
-  .connect('mongodb://127.0.0.1:27017/test_db', {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
